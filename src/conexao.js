ad641c6c8342caaf9baa9f33f96368070ca137dc
rvsfrebeca <rebecaferreiraprofissional@gmail.com>
2021-10-18 20:09:31 -0300

    Corrigindo nome do banco de dados

--- src/conexao.js
+++ src/conexao.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'nxpfcxnsuppwue',
     host: 'ec2-52-70-120-204.compute-1.amazonaws.com',
-    database: 'market_cubos',
+    database: 'dd3fi3b20gpk5c',
     password: 'bb48b77345a329e2d89a3c37ddc1b9cf610a65a25f68d02fd77114319eca2420',
     port: 5432,
     ssl: {
